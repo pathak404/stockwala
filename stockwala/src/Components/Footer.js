c70fa5a577a246180afce4013513083ea9a0179f
Abhishek Kumar Pathak <officialabhishekpathak@gmail.com>
2023-03-08 20:47:45 +0530

    small change

--- stockwala/src/Components/Footer.js
+++ stockwala/src/Components/Footer.js
@@ -5,7 +5,7 @@ import {BsFillSuitHeartFill} from 'react-icons/bs'
 function Footer() {
   return (
     <div className='bg-white hstack align-items-center justify-content-center py-4'>
-      <p className='small mb-0'>Made with <BsFillSuitHeartFill /> by <a href="mailto:officialabhishekpathak@gmail.com" className='text-decoration-none text-dark'>AKP & AR</a></p>
+      <p className='small mb-0'>Made with <BsFillSuitHeartFill /> by <a href="mailto:officialabhishekpathak@gmail.com" className='text-decoration-none text-dark'>Abhishek Pathak</a></p>
     </div>
   )
 }
